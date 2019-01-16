0fdc5ac85f7fbd69d20c005c86dfcfca467f6680
Hampus Näsvall <hampus.nasvall@learnet.se>
2019-01-16 12:44:34 +0100

    uppload

---
+++ gubbe.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.lang.*;
+
+
+public class gubbe {
+
+    public static void main(String[] args) {
+
+    Random random = new Random(2);
+    int number = random.nextInt(10) + 1;
+    String awnser = String.valueOf(number);
+    }
+    if (answer == 9){
+        System.out.println("boii");
+    }
+}
