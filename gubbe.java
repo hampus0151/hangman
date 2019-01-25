a31f9a66ee8100f31c962923443be8e43df37db4
Hampus Nasvall <40735091+hampus0151@users.noreply.github.com>
2019-01-25 09:06:41 +0100

    Add files via upload

--- gubbe.java
+++ gubbe.java
@@ -1,17 +1,76 @@
 import java.util.Scanner;
 import java.util.Random;
-import java.lang.*;
-
 
 public class gubbe {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        String[] guesses = {"distinction","ghost","gas","move","death","dog","welcome","school"};
 
-    public static void main(String[] args) {
+        boolean weArePlaying = true;
+        while (weArePlaying) {
+            System.out.println("Welcome!");
+            char[] randomWordToGuess = guesses[random.nextInt(guesses.length)].toCharArray();
+            int amountOfGuesses = randomWordToGuess.length;
+            char[] playerGuess = new char[amountOfGuesses];
 
-    Random random = new Random(2);
-    int number = random.nextInt(10) + 1;
-    String awnser = String.valueOf(number);
+            for (int i = 0; i < playerGuess.length; i++) {
+                playerGuess[i] = '*';
+            }
+
+            boolean wordIsGuessed = false;
+            int tries = 0;
+
+            while (!wordIsGuessed && tries != amountOfGuesses){
+                System.out.print("current guesses: ");
+                printArray(playerGuess);
+                System.out.printf("you have %d tries left.\n", amountOfGuesses - tries);
+                System.out.println("Enter a single character");
+                char input = scanner.nextLine().charAt(0);
+                tries ++;
+
+                if (input == '-') {
+                    weArePlaying = false;
+                    wordIsGuessed = true;   
+                } else {
+                    for (int i = 0; i < randomWordToGuess.length; i++){
+                        if (randomWordToGuess[i] == input) {
+                            playerGuess[i] = input;
+                        }
+                    }
+                
+                    if (isTheWordGuessed(playerGuess)) {
+                        wordIsGuessed = true;
+                        System.out.println("congrads comrade");
+                    }
+
+                }
+
+            }
+
+            if (!wordIsGuessed) System.out.println("you have no guesses left :(");
+            System.out.println("You want to play again (yes/no)?");
+            String anotherGame = scanner.nextLine();
+            if (anotherGame.equals("no")) weArePlaying = false;
+
+        }
+
+        System.out.println("game over.");
+
     }
-    if (answer == 9){
-        System.out.println("boii");
+
+
+    public static void printArray(char[] array) {
+        for (int i = 0; i < array.length; i++){
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+    
+    public static boolean isTheWordGuessed(char[] array) {
+        for (int i = 0; i < array.length; i++){
+            if (array[i] == '*') return false;
+        }
+        return true;
     }
 }
